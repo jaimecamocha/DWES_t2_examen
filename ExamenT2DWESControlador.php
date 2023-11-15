b06cdf7dcbd7b222804b59ab4ada942888d0811c
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-11-15 11:42:54 +0100

    EXAMEN DEL TEMA 2 DE DWES

---
+++ ExamenT2DWESControlador.php
@@ -0,0 +1,35 @@
+<?php
+
+// solicitar los archivos "articulo.php", "bebida.php", "pizza.php";
+
+// Inicialización de los artículos
+$articulos = [
+    new Articulo("Lasagna", 3.50, 7.00, 20),
+    new Articulo("Pan de ajo y mozzarella", 2.00, 4.50, 15),
+    new Pizza("Pizza Margarita", 4.00, 8.00, 30, ["Tomate", "Mozzarella", "Albahaca"]),
+    new Pizza("Pizza Pepperoni", 5.00, 10.00, 25, ["Tomate", "Mozzarella", "Pepperoni"]),
+    new Pizza("Pizza Vegetal", 4.50, 9.00, 18, ["Tomate", "Mozzarella", "Verduras Variadas"]),
+    new Pizza("Pizza 4 quesos", 5.50, 11.00, 20, ["Mozzarella", "Gorgonzola", "Parmesano", "Fontina"]),
+    new Bebida("Refresco", 1.00, 2.00, 50, false),
+    new Bebida("Cerveza", 1.50, 3.00, 40, true)
+];
+
+// Ejemplo de uso
+
+
+mostrarMenu($articulos);
+mostrarMasVendidos($articulos);
+mostrarMasLucrativos($articulos);
+
+function mostrarMenu($articulos) {
+
+}
+
+function mostrarMasVendidos($articulos) {
+
+}
+
+function mostrarMasLucrativos($articulos) {
+
+}
+
